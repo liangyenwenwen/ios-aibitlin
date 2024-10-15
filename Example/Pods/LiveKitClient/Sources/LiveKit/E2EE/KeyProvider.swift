f42ea8a8c4e375365ba01a1157ff5d21fca66cda
张亚飞 <672233580@qq.com>
2024-10-15 20:00:29 +0800

    update

--- Example/Pods/LiveKitClient/Sources/LiveKit/E2EE/KeyProvider.swift
+++ Example/Pods/LiveKitClient/Sources/LiveKit/E2EE/KeyProvider.swift
@@ -52,7 +52,7 @@ public class BaseKeyProvider: Loggable {
                                                          ratchetWindowSize: options.ratchetWindowSize,
                                                          sharedKeyMode: isSharedKey,
                                                          uncryptedMagicBytes: options.uncryptedMagicBytes,
-                                                         failureTolerance: options.failureTolerance, keyRingSize: 0)
+                                                         failureTolerance: options.failureTolerance)
         if isSharedKey && sharedKey != nil {
             let keyData = sharedKey!.data(using: .utf8)!
             self.rtcKeyProvider?.setSharedKey(keyData, with: 0)
--- Example/Pods/LiveKitClient/Sources/LiveKit/Extensions/RTCI420Buffer.swift
+++ Example/Pods/LiveKitClient/Sources/LiveKit/Extensions/RTCI420Buffer.swift
@@ -62,7 +62,7 @@ internal extension RTCI420Buffer {
                               dstUV: dstUV,
                               dstStrideUV: Int32(dstUVStride),
                               width: width,
-                              height: height)
+                              width: height)
 
         } else {
             let dst = CVPixelBufferGetBaseAddress(outputPixelBuffer)
