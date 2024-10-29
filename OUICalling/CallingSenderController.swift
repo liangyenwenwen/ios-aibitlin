6e6844d6f66d3d41ec9480642a11eda67b2f6790
张亚飞 <672233580@qq.com>
2024-10-29 10:33:08 +0800

    名字恢复

--- OUICalling/CallingSenderController.swift
+++ OUICalling/CallingSenderController.swift
@@ -117,7 +117,7 @@ class SignalViewController: CallingBaseViewController {
         let nameLabel = UILabel()
         nameLabel.layer.cornerRadius = 6
         nameLabel.layer.masksToBounds = true
-        nameLabel.text = inviter?.nickname
+        nameLabel.text = SuperStringUtil.getUserShowname(showname: inviter?.nickname ?? "")
         nameLabel.font = .systemFont(ofSize: 28)
         nameLabel.textAlignment = .center
         nameLabel.textColor = .white
@@ -617,3 +617,56 @@ extension RoomViewController: RoomDelegate {
     }
 }
 
+
+
+class SuperStringUtil {
+    
+  
+    /// 获取用户的信息  博客 公司 vip 名字
+    static func getUserState(showname: String) -> UserState {
+        guard let jsonData = showname.data(using: .utf8) else { return UserState(b: 0, e: 0, v: 0, n: showname)}
+        do {
+            let user = try JSONDecoder().decode(UserState.self, from: jsonData)
+            return user
+        } catch {
+            return  UserState(b: 0, e: 0, v: 0, n: showname)
+        }
+    }
+    
+    static func getUserShowname(showname: String) -> String  {
+        let user = getUserState(showname: showname)
+        return user.n
+    }
+    /// 获取用户的tag
+    static func getUserTag(showname: String) -> String? {
+        guard let jsonData = showname.data(using: .utf8) else { return nil}
+        do {
+            let user = try JSONDecoder().decode(UserState.self, from: jsonData)
+            var reslut = ""
+            if user.v > 0 {
+                reslut.append("V\(user.v)")
+            }
+            
+            if user.b > 0 {
+                reslut.append(reslut.count == 0 ? "\("博客".localized())" : "、\("博客".localized())")
+            }
+            
+            if user.e > 0 {
+                reslut.append(reslut.count == 0 ? "\("企业".localized())" : "、\("企业".localized())")
+            }
+            
+            return reslut.count == 0 ? nil : "[\(reslut)]"
+        } catch {
+            return  nil
+        }
+    }
+    
+}
+
+
+struct UserState: Codable {
+    let b: Int
+    let e: Int
+    let v: Int
+    let n: String
+}
